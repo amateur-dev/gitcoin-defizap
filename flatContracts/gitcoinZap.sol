02a159605f6c7aa820d5768ab172072faecb5cdc
amateur-dev <dipesh.sukhani@gmail.com>
2020-03-26 00:58:09 +0800

    refacotring the flattened file

--- flatContracts/gitcoinZap.sol
+++ flatContracts/gitcoinZap.sol
@@ -1,3 +1,30 @@
+// Copyright (C) 2019, 2020 dipeshsukhani, nodarjonashia, suhailg
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// Visit <https://www.gnu.org/licenses/>for a copy of the GNU Affero General Public License
+
+/**
+ * WARNING: This is an upgradable contract. Be careful not to disrupt
+ * the existing storage layout when making upgrades to the contract. In particular,
+ * existing fields should not be removed and should not have their types changed.
+ * The order of field declarations must not be changed, and new fields must be added
+ * below all existing declarations.
+ *
+ * The base contracts and the order in which they are declared must not be changed.
+ * New fields must not be added to base contracts (unless the base contract has
+ * reserved placeholder fields for this purpose).
+ *
+ * See https://docs.zeppelinos.org/docs/writing_contracts.html for more info.
+*/
 
 // File: @openzeppelin/upgrades/contracts/Initializable.sol
 
@@ -453,33 +480,7 @@ library SafeERC20 {
 
 pragma solidity ^0.5.0;
 
-// Copyright (C) 2019, 2020 dipeshsukhani, nodarjonashia, suhailg
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// Visit <https://www.gnu.org/licenses/>for a copy of the GNU Affero General Public License
-
-/**
- * WARNING: This is an upgradable contract. Be careful not to disrupt
- * the existing storage layout when making upgrades to the contract. In particular,
- * existing fields should not be removed and should not have their types changed.
- * The order of field declarations must not be changed, and new fields must be added
- * below all existing declarations.
- *
- * The base contracts and the order in which they are declared must not be changed.
- * New fields must not be added to base contracts (unless the base contract has
- * reserved placeholder fields for this purpose).
- *
- * See https://docs.zeppelinos.org/docs/writing_contracts.html for more info.
-*/
 
 interface oneSplit {
     function goodSwap(
